465271acbbd855bc90df7b0c3f682eeae0b5c819
Kaja Solberg Sandnes <kajassa@uio.no>
2025-10-09 17:11:39 +0200

    Importert fra ast2000-tools

---
+++ Del4/space_rocket_velocity.py
@@ -0,0 +1,15 @@
+# Ikke brukt kodemal
+import ast2000tools.utils as utils
+import ast2000tools.constants as const
+seed = utils.get_seed('oafincke')
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ast2000tools.solar_system import SolarSystem
+system = SolarSystem(seed)
+from ast2000tools.space_mission import SpaceMission
+mission = SpaceMission(seed)
+
+
+
+    
